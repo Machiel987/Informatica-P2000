af0d0ae6b41671f7dd12adfbf3c3d008d89e79e4
Jasper van der Geer <jasper9@pulsar.nl>
2024-12-09 08:33:19 +0100

    Added shell files

---
+++ compile.sh
@@ -0,0 +1,21 @@
+# Don't forget to set environment variables!
+
+# Compile
+	zcc \
+	+embedded -clib=sdcc_iy \
+	main.c graphics.c game_of_life.c utils.c \
+	-startup=2 \
+	-pragma-define:CRT_ORG_CODE=0x1000 \
+	-pragma-define:CRT_ORG_DATA=0x6500 \
+	-pragma-define:REGISTER_SP=0x9FFF \
+	-pragma-define:CRT_STACK_SIZE=256 \
+	-pragma-define:CRT_INCLUDE_PREAMBLE=1 \
+	-pragma-define:CLIB_FOPEN_MAX=0 \
+	-SO3 \
+	-bn main.bin \
+	--max-allocs-per-node10000 \
+	--fsigned-char \
+	-create-app -m
+
+rm main.bin
+mv main.rom main.bin
---
+++ environment.sh
@@ -0,0 +1,4 @@
+# Sets environment variables
+
+export ZCCCFG="C:\Users\bas\z88dk\lib\config"
+export PATH="C:\Users\bas\z88dk\bin":$PATH
